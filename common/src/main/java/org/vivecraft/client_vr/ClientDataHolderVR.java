a8b1ac525b6048a27ae8a0125c90c58e627dca27
Aram <schonhagearam@gmail.com>
2025-12-05 16:55:29 +0100

    revert

--- common/src/main/java/org/vivecraft/client_vr/ClientDataHolderVR.java
+++ common/src/main/java/org/vivecraft/client_vr/ClientDataHolderVR.java
@@ -186,7 +186,7 @@ public class ClientDataHolderVR {
      * @param <T>         Class of the tracker
      * @return created tracker instance
      */
-    public <T extends Tracker> T createTracker(BiFunction<Minecraft, ClientDataHolderVR, T> constructor) {
+    private <T extends Tracker> T createTracker(BiFunction<Minecraft, ClientDataHolderVR, T> constructor) {
         T tracker = constructor.apply(Minecraft.getInstance(), this);
         registerTracker(tracker);
         return tracker;
